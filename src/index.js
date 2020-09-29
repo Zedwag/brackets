9e4638bb82dc98a344c084530162b2deae53fb89
Zedwag <zedwag@gmail.com>
2020-09-30 01:03:28 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = []
+    bracketsConfig = [].concat(...bracketsConfig)
+    str.split('').forEach(el => el === stack[stack.length - 1] ? stack.pop() : stack.push(bracketsConfig[bracketsConfig.indexOf(el) + 1]))
+    return stack.length === 0 ? true : false
 }
